fc34df71345919cf3fceb0c44721e7b0ce80f0e0
LeonardoFariaOliveira <leonardoN00Bfaria@hotmail.com>
2022-08-14 12:49:34 -0300

    Done

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{html,tsx, ts}"],
+  theme: {
+    fontFamily: {
+      'body': ['"Kanit"'],
+    }
+  },
+  plugins: [],
+}
